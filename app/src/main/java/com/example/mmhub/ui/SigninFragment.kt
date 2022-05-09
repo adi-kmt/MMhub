b7c8459dcddf2f9b8664dca15d1c382b2372e2ef
Adithya Kamath <kamathis4@gmail.com>
2022-05-09 17:49:13 +0530

    Initial folder structure

---
+++ app/src/main/java/com/example/mmhub/ui/SigninFragment.kt
@@ -0,0 +1,23 @@
+package com.example.mmhub.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mmhub.R
+
+class SigninFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signin, container, false)
+    }
+}
